7fb620c4d20ed483cdd8450083d409a185c895e2
obihz6 <wuzhibo6@gmail.com>
2026-09-26 15:54:07 +0200

    added monel400 hook to firstdegreematerialsB

--- groovy/material/FirstDegreeMaterialsB.groovy
+++ groovy/material/FirstDegreeMaterialsB.groovy
@@ -1126,5 +1126,7 @@ class FirstDegreeMaterialsB {
                 .colorAverage()
                 .iconSet(SHINY)
                 .build()
+
+        Monel400 = supersymmetry.common.materials.SusyMaterials.Monel400
     }
 }
